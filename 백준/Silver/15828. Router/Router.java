9465016132d66f3e2952e3de5bd00244d370f6a2
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2023-09-30 22:06:44 +0900

    [Silver IV] Title: Router, Time: 664 ms, Memory: 33640 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        while (true){
+            int input = Integer.parseInt(br.readLine());
+            // 0이 들어왔을 때(맨 앞 원소 제거)
+            if(input==0)
+                queue.poll();
+            // -1이 들어왔을 때(break)
+            else if(input==-1)
+                break;
+            // 나머지 수 들어왔을 때
+            else
+                if(queue.size()<=n) // 버퍼 크기만큼만 입력 받기
+                    queue.offer(input);
+        }
+        
+        if(queue.size()==0)
+            System.out.println("empty");
+        else {
+            while (queue.size() > 0)
+                System.out.print(queue.poll() + " ");
+        }
+    }
+}
