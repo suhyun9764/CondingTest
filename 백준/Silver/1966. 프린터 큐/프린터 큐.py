ebb51e8449fe2710922a99d4d45e7225392956bf
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-08 00:18:33 +0900

    [Silver III] Title: 프린터 큐, Time: 140 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,29 +1,28 @@
-from collections import deque
-
-
-class Printer:
-    def __init__(self, arr):
-        self.arr = deque(arr)
-    def get_order(self, idx):
-        pri_arr = deque(enumerate(self.arr))
-        cnt = 0
-        while pri_arr:
-            current_document = pri_arr.popleft()
-            if any(current_document[1] < document[1] for document in pri_arr):
-                pri_arr.append(current_document)
-            else :
-                cnt+=1
-                if current_document[0]==idx:
-                    return cnt
-
-
-t = int(input())
-
-for _ in range(t):
-    n, m = map(int, input().split())
-    arr = list(map(int, input().split()))
-
-    printer = Printer(arr)
-    print(printer.get_order(m))
-
-
+from collections import deque                                 
+                                                              
+                                                              
+class Solution:                                               
+    def get_docNum(self, targetNum: int, docs: list):         
+        prior_table = sorted(docs)                            
+        queue = deque(enumerate(docs))                        
+        cnt = 0                                               
+        while queue:                                          
+            current_target_prior = prior_table[-1]            
+            if queue[0][1] == current_target_prior :          
+                cnt+=1                                        
+                if(queue[0][0] == targetNum):                 
+                    break                                     
+                queue.popleft()                               
+                prior_table.pop()                             
+                                                              
+            else :                                            
+                queue.append(queue.popleft())                 
+                                                              
+        return cnt                                            
+                                                              
+t = int(input())                                              
+sol = Solution()                                              
+for _ in range(t):                                            
+    n,m = map(int,input().split(" "))                         
+    print(sol.get_docNum(m,list(input().split(" "))))         
+                                                              
