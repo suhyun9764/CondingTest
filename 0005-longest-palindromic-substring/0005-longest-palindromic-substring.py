e7900d36d123f9519b7a4ef4d1ba25e078d50ba7
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-07 15:16:06 +0900

    Time: 277 ms (85.58%), Space: 16.6 MB (69.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,33 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s)==1:
-            return s
-        
+        s_length = len(s)
+        max_length = 1
         start = 0
-        max = 1
-        
-        for i in range(len(s)):
-            start_idx = i-1
-            end_idx = i+1
-            
-            while start_idx>=0 and end_idx<len(s) and s[start_idx] == s[end_idx]:
-                if end_idx-start_idx+1> max:
-                    start = start_idx
-                    max = end_idx-start_idx+1
-                    
-                start_idx-=1
-                end_idx+=1
-                
-            start_idx = i
-            end_idx = i+1
+        if s_length == 1:
+            return s
 
-            while start_idx >= 0 and end_idx < len(s) and s[start_idx] == s[end_idx]:
-                if end_idx-start_idx+1> max:
-                    start = start_idx
-                    max = end_idx-start_idx+1
-                    
-                start_idx -=1
-                end_idx +=1
-        
-        return s[start:start+max]
-            
+        for i in range(s_length):
+            left = i - 1
+            right = i + 1
+            while (left >= 0 and right < s_length and s[left] == s[right]):
+                if max_length < right - left + 1:
+                    max_length = right - left + 1
+                    start = left
+                left -= 1
+                right += 1
+
+            left = i
+            right = i + 1
+            while (left >= 0 and right < s_length and s[left] == s[right]):
+                if max_length < right - left + 1:
+                    max_length = right - left + 1
+                    start = left
+
+                left -= 1
+                right += 1
+
+        return s[start:start + max_length]
