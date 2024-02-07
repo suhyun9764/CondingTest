288669209735af2d745dd92c74a38e10cc0cb450
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-07 16:05:11 +0900

    Time: 224 ms (55.81%), Space: 19.7 MB (78.84%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -2,6 +2,5 @@ from typing import List
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        sorted_list = sorted(nums)
-    
-        return sum(sorted_list[::2])
+        nums.sort()
+        return sum(nums[::2])
