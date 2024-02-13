d5b0869ec22e649afc95e6307e3132291da91ae8
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-02-13 09:48:42 +0900

    Time: 348 ms (63.48%), Space: 16.8 MB (60.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,34 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        s_length = len(s)
-        max_length = 1
+        max = 1
         start = 0
-        if s_length == 1:
+        if len(s) <= 1:
             return s
-
-        for i in range(s_length):
-            left = i - 1
-            right = i + 1
-            while (left >= 0 and right < s_length and s[left] == s[right]):
-                if max_length < right - left + 1:
-                    max_length = right - left + 1
-                    start = left
-                left -= 1
-                right += 1
-
+        for i in range(len(s)):
+            # 짝수일 때
             left = i
             right = i + 1
-            while (left >= 0 and right < s_length and s[left] == s[right]):
-                if max_length < right - left + 1:
-                    max_length = right - left + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max:
+                    max = right - left + 1
                     start = left
+                left -=1
+                right +=1
 
-                left -= 1
-                right += 1
 
-        return s[start:start + max_length]
+            # 홀수일 때
+            left = i-1
+            right = i+1
+            while left >= 0 and right < len(s) and s[left]==s[right]:
+                if right - left + 1 > max:
+                    max = right - left + 1
+                    start = left
+                left -=1
+                right +=1
+
+        return s[start:start+max]
+
+
+sol = Solution()
+s = " "
+print(sol.longestPalindrome(s))
