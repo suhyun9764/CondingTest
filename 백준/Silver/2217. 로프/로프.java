6edf7ec96f5d46784d3ab06ce1b5e5157dc0f66d
admiNHyun <80302833+suhyun9764@users.noreply.github.com>
2024-06-17 22:50:14 +0900

    [Silver IV] Title: 로프, Time: 332 ms, Memory: 29504 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -5,26 +5,23 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] ropes = new int[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            ropes[i] = Integer.parseInt(br.readLine());
-        }
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		int maxWeight = 0;
+		for (int i = 0; i < n; i++) {
+			int currentWeight = arr[i] * (n - i);
+			if (maxWeight < currentWeight)
+				maxWeight = currentWeight;
+		}
 
-        Arrays.sort(ropes); // 오름차순 정렬
+		System.out.println(maxWeight);
 
-        int maxWeight = 0;
-        for (int i = 0; i < n; i++) {
-            // (n - i)개의 로프가 현재의 로프를 사용할 때 최대 중량을 계산
-            int currentWeight = ropes[i] * (n - i);
-            if (currentWeight > maxWeight) {
-                maxWeight = currentWeight;
-            }
-        }
-
-        System.out.println(maxWeight);
-    }
+	}
 }
