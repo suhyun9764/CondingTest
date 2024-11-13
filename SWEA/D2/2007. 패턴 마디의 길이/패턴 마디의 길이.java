70abc890b46c1f994598643031435eb1521a71aa
suhyun <80302833+suhyun9764@users.noreply.github.com>
2024-11-13 18:25:28 +0900

    [D2] Title: 패턴 마디의 길이, Time: 99 ms, Memory: 18,452 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -1,30 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수 읽기
-        
-        for (int t = 1; t <= T; t++) {
-            String input = br.readLine(); // 테스트 케이스 문자열 입력
-            
-            int patternLength = 0;
-            // 마디 길이 후보 1부터 10까지 확인
-            for (int len = 1; len <= 10; len++) {
-                String pattern = input.substring(0, len); // 길이 len의 부분 문자열
-                
-                // 부분 문자열이 전체 문자열의 패턴이 되는지 확인
-                if (input.startsWith(pattern + pattern)) {
-                    patternLength = len;
-                    break;
-                }
-            }
-            
-            // 결과 출력
-            System.out.println("#" + t + " " + patternLength);
-        }
-    }
+	private static int T;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 테스트 케이스 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		// 테스트 순회
+		for(int i=0;i<T;i++) {
+			String inputValue = br.readLine();
+			int answer = 0;
+			// 패턴 찾기
+			answer = findPattern(inputValue, answer);
+			System.out.println("#"+(i+1)+" "+answer);
+		}
+	}
+
+	private static int findPattern(String inputValue, int answer) {
+		for(int length=1;length<10;length++) {
+			String pattern = inputValue.substring(0,length);
+			if(inputValue.startsWith(pattern+pattern)){
+				answer=length;
+				break;
+			}
+		}
+		return answer;
+	}
+
 }
