bf164130ffb75ea6a951fa48cb960b2a5bddd36c
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-02-12 14:37:10 +0900

    [level 2] Title: 구명보트, Time: 14.23 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+        public int solution(int[] people, int limit) {
+            Arrays.sort(people);
+            int start = 0;
+            int end = people.length - 1;
+            int answer = 0;
+            while (start <= end) {
+                if (people[start] + people[end] <= limit) {
+                    start++;
+                }
+                end--;
+                answer++;
+            }
+            return answer;
+        }
+    }
