3757dd46772083c7a272cc9444e8054d6de5dc42
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-02-12 23:46:09 +0900

    [level 2] Title: 주식가격, Time: 15.74 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-
-            for(int i=0;i<prices.length;i++){
-                int currentPrice = prices[i];
-                for(int j=i+1;j<prices.length;j++){
-                    if(currentPrice>prices[j]){
-                        answer[i] = j-i;
-                        break;
-                    }
-                }
-                if(answer[i]==0)
-                    answer[i] = prices.length-1-i;
-            }
-            return answer;
+        int[] answer = {};
+        return answer;
     }
 }
