9d1eabf8b2cbd26df5d7a86b0d95cf19e088f2c5
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-02-16 17:28:26 +0900

    [level 3] Title: 표 편집, Time: 213.02 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,65 +1,69 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-
 class Solution {
         public String solution(int n, int k, String[] cmd) {
-            int[] prev = new int[n];
-            int[] next = new int[n];
-            int cursor = k;
-            Deque<Integer> deleteStack = new ArrayDeque<>();
+            Deque<Integer> inRangeStack = new ArrayDeque<>();   // 커서 포함 위쪽
+            Deque<Integer> outRangeStack = new ArrayDeque<>();  // 커서 밑쪽
+            Deque<Integer> deleteStack = new ArrayDeque<>();    // 삭제된 로그
+            boolean[] isDeleted = new boolean[n];   // 삭제돤 행 체크
 
-            for(int i=0;i<n;i++){
-                prev[i] = i-1;
-                next[i] = i+1;
+            // 현재 커서기준으로 위쪽 아랫쪽 채우기
+            for (int i = 0; i < k+1 ; i++) {   
+                inRangeStack.push(i);
             }
 
-            next[n-1] = -1;
+            for (int i = n; i > k; i--) {
+                outRangeStack.push(i);
+            }
 
             for (String str : cmd) {
                 String[] orderAndNumber = str.split(" ");
                 if (orderAndNumber[0].equals("U")) {
                     int number = Integer.parseInt(orderAndNumber[1]);
-                    for(int i=0;i<number;i++){
-                        cursor = prev[cursor];
+                    for (int i = 0; i < number; i++) {
+                        // 만약 위쪽 행이 삭제된 항목일 경우 카운트에 포함하지 않고 넘기기
+                        while (!inRangeStack.isEmpty()&&isDeleted[inRangeStack.peek()]){
+                            outRangeStack.push(inRangeStack.pop());
+                        }
+                        // 현재 커서가 최상단일 경우 
+                        if (inRangeStack.isEmpty())
+                            break;
+
+                        outRangeStack.push(inRangeStack.pop());
+                        k--;
                     }
                 }
                 if (orderAndNumber[0].equals("D")) {
                     int number = Integer.parseInt(orderAndNumber[1]);
-                    for(int i=0;i<number;i++){
-                        cursor = next[cursor];
+                    for (int i = 0; i < number; i++) {
+                        while (!outRangeStack.isEmpty()&&isDeleted[outRangeStack.peek()]){
+                            inRangeStack.push(outRangeStack.pop());
+                        }
+                        if (outRangeStack.isEmpty())
+                            break;
+                        inRangeStack.push(outRangeStack.pop());
+                        k++;
                     }
                 }
                 if (orderAndNumber[0].equals("C")) {
-                    deleteStack.push(cursor);
-                    int prevIndex = prev[cursor];
-                    int nextIndex = next[cursor];
-
-                    if(prevIndex!=-1) next[prevIndex] = nextIndex;
-                    if(nextIndex!=-1) prev[nextIndex] = prevIndex;
-
-                    if(nextIndex!=-1){
-                        cursor=nextIndex;
-                    }else{
-                        cursor=prevIndex;
-                    }
+                    isDeleted[k] = true;
+                    deleteStack.push(k);
                 }
                 if (orderAndNumber[0].equals("Z")) {
                     int latestDelete = deleteStack.pop();
-                    int prevIndex = prev[latestDelete];
-                    int nextIndex = next[latestDelete];
-
-                    if(prevIndex!=-1) next[prevIndex] = latestDelete;
-                    if(nextIndex!=-1) prev[nextIndex] = latestDelete;
+                    isDeleted[latestDelete] = false;
                 }
             }
 
-            StringBuilder answer = new StringBuilder("O".repeat(n));
-            while (!deleteStack.isEmpty()){
-                int currentDelete = deleteStack.pop();
-                answer.setCharAt(currentDelete,'X');
+            StringBuilder answer = new StringBuilder();
+            for (boolean b : isDeleted) {
+                if(b){
+                    answer.append("X");
+                }else{
+                    answer.append("O");
+                }
             }
-
             return answer.toString();
         }
     }
