a7d83608f2e83ba8c3f3179f82dddc0e0f38d002
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-02-19 09:52:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 106.70 ms, Memory: 98.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+        public String solution(String[] participant, String[] completion) {
+            Map<String,Integer> map = new HashMap<>();
+            for (String name : participant) {
+                map.put(name,map.getOrDefault(name,0)+1);
+            }
+
+            for (String name : completion) {
+                map.put(name, map.get(name)-1);
+            }
+
+            for (String name : map.keySet()) {
+                if(map.get(name)!=0)
+                    return name;
+                
+            }
+            return null;
+        }
+    }
