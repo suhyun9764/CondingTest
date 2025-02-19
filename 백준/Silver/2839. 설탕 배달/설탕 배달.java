5dd461262c9adcb7e66505fdfd72a6463c8f9514
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-02-19 14:30:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,6 +1,8 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     static int MIN_BAG = 3;
@@ -9,8 +11,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int answer = getAnswer(N);
-        System.out.println(answer);
+//        int answer = getAnswer(N);
+//        System.out.println(answer);
+        System.out.println(dp(N));
+    }
+
+
+    private static int dp(int n) {
+        int[] arr = new int[n+1];
+        Arrays.fill(arr,5001);
+        if(n<=5){
+            if(n==3||n==5) return 1;
+            return -1;
+        }
+        // 초기값 설정
+        arr[3] = 1;
+        arr[5] = 1;
+
+        for(int i=6;i<=n;i++){
+            arr[i] = Math.min(arr[i-3],arr[i-5])+1;
+        }
+        return arr[n]>=5001?-1:arr[n];
     }
 
     private static int getAnswer(int n) {
