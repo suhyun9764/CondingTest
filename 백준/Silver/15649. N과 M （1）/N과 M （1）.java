feaeccac2e43503a52d7737a26f8f27fe220cc3e
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-04-28 22:20:14 +0900

    [Silver III] Title: N과 M (1), Time: 1704 ms, Memory: 66664 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,43 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static boolean[] visited;
-    static int[] result;
+    static int N,M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        sc.close();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N + 1]; // 방문 체크 배열
-        result = new int[M]; // 결과를 저장할 배열
+        List<Integer> list = new ArrayList<>();
+        boolean[] visit = new boolean[N+1];
+        dfs(0,list,visit);
 
-        backtrack(0);
     }
 
-    static void backtrack(int depth) {
-        if (depth == M) { // M개를 모두 선택했을 경우 출력
-            for (int num : result) {
-                System.out.print(num + " ");
+    private static void dfs(int count, List<Integer> list, boolean[] visit) {
+        if(count==M){
+            for (Integer i : list) {
+                System.out.print(i+" ");
             }
             System.out.println();
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) { // 아직 선택하지 않은 숫자라면
-                visited[i] = true; // 방문 체크
-                result[depth] = i; // 현재 위치에 숫자 저장
-                backtrack(depth + 1); // 다음 숫자 선택
-                visited[i] = false; // 백트래킹 (원상 복구)
-            }
+        for(int i=1;i<=N;i++){
+            if(visit[i]) continue;
+            visit[i] = true;
+            list.add(i);
+            dfs(count+1,list,visit);
+            list.remove(list.size()-1);
+            visit[i] = false;
         }
+
     }
 }
