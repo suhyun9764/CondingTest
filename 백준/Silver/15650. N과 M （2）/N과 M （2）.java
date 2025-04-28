4f265b65c8c6311cbf5a5154405a72a43a51af13
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-04-28 22:28:49 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,39 +1,42 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-
+    static int N,M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[] result = new int[M];
-        dfs(0, result,1);
+
+        List<Integer> list = new ArrayList<>();
+        boolean[] visit = new boolean[N+1];
+        dfs(1, 0,list,visit);
 
     }
 
-    private static void dfs(int count, int[] result,int start) {
-        if (count == M) {
-            for (int i : result) {
-                System.out.print(i + " ");
+    private static void dfs(int current, int count, List<Integer> list, boolean[] visit) {
+        if(count==M){
+            for (Integer i : list) {
+                System.out.print(i+" ");
             }
             System.out.println();
             return;
         }
 
-        if(start>N)
+        if(current>N)
             return;
 
-        result[count] =start;
-        dfs(count+1,result,start+1);
-        result[count] = 0;
-        dfs(count,result,start+1);
-
+        visit[current] = true;
+        list.add(current);
+        dfs(current+1,count+1,list,visit);
+        visit[current] = false;
+        list.remove(list.size()-1);
+        dfs(current+1,count,list,visit);
     }
-
 }
