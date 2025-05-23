d0261154e95c4077dc16f6c20e3e0878764cb740
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-05-24 04:49:23 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,40 @@
+class Solution {
+        public String solution(String s) {
+            char[] charArr = s.toCharArray();
+            char before = ' ';
+            for(int i=0;i<charArr.length;i++){
+                // 앞이 숫자이거나 빈칸인 경우
+                if(before==' '){
+                    // 현재가 숫자인 경우
+                    before = charArr[i];
+                    if(charArr[i]>'z'||charArr[i]<'A'||charArr[i]==' '){
+                        continue;
+                    }
+
+                    // 현재가 소문자인 경우
+                    if('a'<=charArr[i]&& charArr[i]<='z'){
+                        charArr[i] = (char)(charArr[i] + ('A'-'a'));
+                    }
+                    before = charArr[i];
+                }
+                // 아닌 경우
+                else{
+                    // 현재가 숫자나 빈칸인 경우
+                    if(charArr[i]>'z'||charArr[i]<'A'||charArr[i]==' '){
+                        before = ' ';
+                        continue;
+                    }
+                    // 현재가 대문자인 경우
+                    if('A'<=charArr[i]&& charArr[i]<='Z'){
+                        charArr[i] = (char)(charArr[i] - ('A'-'a'));
+                    }
+                    before = charArr[i];
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<charArr.length;i++){
+                sb.append(charArr[i]);
+            }
+            return sb.toString();
+        }
+    }
