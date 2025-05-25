def0a844bb3d4e782f82d68631a90ca83e6aed05
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-05-25 19:44:05 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int allBoard = brown+yellow;
+        int[] answer = new int[2];
+        for(int i=3;i<=allBoard/2;i++){
+            if(allBoard%i==0){
+                int j = allBoard/i;
+                if((i-2)*(j-2)==yellow){
+                        answer[0] = Math.max(i,j);
+                        answer[1] = Math.min(i,j);
+                        break;
+                    }
+            }          
+        }
+                return answer;
+    }
+}
