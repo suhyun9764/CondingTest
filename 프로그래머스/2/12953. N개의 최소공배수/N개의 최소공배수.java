42442a175ab642e731d04c48a28a6071e5b9c8a9
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-05-27 10:54:20 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.41 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        // arr 오름차순 정렬
+        Arrays.sort(arr);
+        // arr 순회
+        int answer = arr[0];
+        for(int i=1;i<arr.length;i++){
+            int salt = 2;
+            int min = Math.min(answer,arr[i]);
+            int max = Math.max(answer,arr[i]);
+            // arr[i+1]%arr[i] ==0 이될때까지 arr * 2,3 ..
+            int buff = max;
+            while(buff%min!=0){
+                buff = max*salt;
+                salt++;
+            }
+                    // 최소공배수 업데이트
+            answer = buff;
+        }
+
+        return answer;
+    }
+}
