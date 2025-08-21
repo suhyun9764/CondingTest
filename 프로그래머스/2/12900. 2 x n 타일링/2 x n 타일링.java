61976c0e0171cf612f087f315f34bc5a41637df7
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-08-22 00:40:04 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.55 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,15 +1,6 @@
 class Solution {
     public int solution(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        long a =1;
-        long b =2;
-        for(int i=3;i<=n;i++){
-            long cur = (a+b) % 1000000007;
-            a = b;
-            b = cur;
-        }
-        
-        return (int)b;
+        int answer = 0;
+        return answer;
     }
 }
