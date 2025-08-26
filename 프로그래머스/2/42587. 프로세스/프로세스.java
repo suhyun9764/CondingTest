6a53fffc07306a94d5cb60d9e6bbe2db9edc1e45
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-08-26 17:09:22 +0900

    [level 2] Title: 프로세스, Time: 1.86 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,39 +1,48 @@
 import java.util.*;
 
+
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        Queue<Task> q = new ArrayDeque<>();
-        
+
+        // Queue 생성
+        Queue<Process> queue = new ArrayDeque<>();
+        // priorities 순회
         for(int i=0;i<priorities.length;i++){
-            pq.offer(priorities[i]);
-            q.offer(new Task(priorities[i],i));
+                    // 큐에 삽입
+            queue.add(new Process(i,priorities[i]));
         }
+        // priorities 정렬
+        Arrays.sort(priorities);
+        // location이 나올때까지 while
+        int cur = -1;
+        int turn = 0;
         
-        int answer = 0;
-        int num = 0;
-        while(pq.size()!=0){
-            int currentPr = pq.poll();
-            while(currentPr!=q.peek().pr){
-                Task tsk = q.poll();
-                q.offer(tsk);
+        int answer = -1;
+        while(!queue.isEmpty()){
+            Process process = queue.poll();
+            if(process.priority!=priorities[priorities.length-1-turn]){
+                queue.add(process);
+                continue;
             }
-            Task currentTask = q.poll();
-            num++;
-            if(currentTask.order == location)
-                answer = num;
+            
+            turn++;
+            if(process.order==location){
+                answer = turn;
+                break;
+            }
         }
+        // 현재 poll한 값이 currentPriority가 아닌 경우 push
         
         return answer;
     }
 }
-
-class Task{
-        int pr;
-        int order;
-        
-        public Task(int pr, int order){
-            this.pr = pr;
-            this.order =order;
-        }
+// process 객체 생성
+class Process{
+    int order;
+    int priority;
+    
+    public Process(int order, int priority){
+        this.order = order;
+        this.priority = priority;
     }
+}
