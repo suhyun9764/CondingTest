070485be7d16a22dab2b4076a4ed2a5be50f8355
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-08-26 17:09:38 +0900

    [level 2] Title: 프로세스, Time: 1.86 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,48 +1,6 @@
-import java.util.*;
-
-
 class Solution {
     public int solution(int[] priorities, int location) {
-
-        // Queue 생성
-        Queue<Process> queue = new ArrayDeque<>();
-        // priorities 순회
-        for(int i=0;i<priorities.length;i++){
-                    // 큐에 삽입
-            queue.add(new Process(i,priorities[i]));
-        }
-        // priorities 정렬
-        Arrays.sort(priorities);
-        // location이 나올때까지 while
-        int cur = -1;
-        int turn = 0;
-        
-        int answer = -1;
-        while(!queue.isEmpty()){
-            Process process = queue.poll();
-            if(process.priority!=priorities[priorities.length-1-turn]){
-                queue.add(process);
-                continue;
-            }
-            
-            turn++;
-            if(process.order==location){
-                answer = turn;
-                break;
-            }
-        }
-        // 현재 poll한 값이 currentPriority가 아닌 경우 push
-        
+        int answer = 0;
         return answer;
-    }
-}
-// process 객체 생성
-class Process{
-    int order;
-    int priority;
-    
-    public Process(int order, int priority){
-        this.order = order;
-        this.priority = priority;
     }
 }
