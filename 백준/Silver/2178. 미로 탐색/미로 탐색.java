b385edce9e729872e51146e7a7e54de1e6341570
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-08-26 23:04:15 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 15640 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,55 +6,46 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
-    static int[][] board;
-    static boolean[][] visit;
-    static int answer;
+    static int[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st  = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
 
-        answer = N*M;
-        board = new int[N][M];
-        visit = new boolean[N][M];
-        for(int i=0;i<N;i++){
-            String[] split = br.readLine().split("");
-            for(int j=0;j<M;j++){
-                board[i][j] = Integer.parseInt(split[j]);
+        for (int i = 0; i < N; i++) {
+            char[] charArray = br.readLine().toCharArray();
+            for (int j = 0; j < charArray.length; j++) {
+                map[i][j] = Integer.parseInt(String.valueOf(charArray[j]));
             }
         }
-        bfs();
-        System.out.println(answer);
-    }
 
-    private static void bfs() {
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{0,0,1});
-        visit[0][0] = true;
+        queue.add(new int[]{0, 0});
 
-        while (!queue.isEmpty()){
-            int[] current = queue.poll();
-            int count = current[2];
-            int y = current[0];
-            int x = current[1];
-            if(y ==N-1&& x ==M-1){
-                answer = count;
-                return;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curY = cur[0];
+            int curX = cur[1];
+
+            if (curY == N - 1 && curX == M - 1) {
+                System.out.println(map[curY][curX]);
+                break;
             }
 
-            int[] dy= {-1,1,0,0};
-            int[] dx ={0,0,-1,1};
-            for(int i=0;i<4;i++){
-                int ny = y+dy[i];
-                int nx = x+dx[i];
-                if(ny<0||nx<0||ny>=N||nx>=M||board[ny][nx]==0||visit[ny][nx]) continue;
-                visit[ny][nx] = true;
-                queue.add(new int[]{ny,nx,count+1});
+            int[] dy = {-1, 1, 0, 0};
+            int[] dx = {0, 0, -1, 1};
+
+            for (int d = 0; d < 4; d++) {
+                int ny = curY + dy[d];
+                int nx = curX + dx[d];
+
+                if (ny < 0 || ny >= N || nx < 0 || nx >= M || map[ny][nx] != 1) continue;
+                map[ny][nx] = map[curY][curX] + 1;
+                queue.add(new int[]{ny, nx});
             }
         }
     }
-
 }
