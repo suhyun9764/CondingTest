8b11dfd16db47ab655efb84be44c652de9ff1608
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-08-27 20:56:51 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 9.34 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,45 +1,55 @@
+
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int n = queue1.length;
-        int[] arr = new int[n*2];
-        
+        // 큐1, 큐2합 각각 구하기
         long sum1 = 0;
-        for(int i=0;i<n;i++){
-            sum1 += queue1[i];
-            arr[i] = queue1[i];
+        for(int i=0;i<queue1.length;i++){
+            sum1+=queue1[i];    
         }
         
         long sum2 = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<queue2.length;i++){
             sum2 += queue2[i];
-            arr[n+i] = queue2[i];
+        }
+        // 큐1+큐2합 구하기
+        long total = sum1+sum2;
+        // 만약 2로 나누어떨어지지 않으면 -1 리턴
+        if(total%2!=0)
+            return -1;
+        // target = 큐1+큐2 / 2
+        long target = total/2;
+        // 큐두개 이어붙혀서 배열만들기
+        int[] arr = new int[queue1.length*2];
+        for(int i=0;i<queue1.length;i++){
+            arr[i] = queue1[i];
         }
         
-        if((sum1+sum2)%2!=0) return -1;
-        long target = (sum1+sum2)/2;
-        
+        for(int i=0;i<queue1.length;i++){
+            arr[queue1.length+i] = queue2[i];
+        }
+        // left = 큐1의 처음, right = 큐2의 처음
         int left = 0;
-        int right = n;
-    
-        int cal = 0;
+        int right = queue1.length;
         
-        while(left<n*2&&right<n*2){
+        int cnt = 0;
+        // 큐1의 합이 크다면 left++, 작다면 right ++
+        while(left<arr.length&&right<arr.length){
             if(sum1==target)
-                return cal;
+                return cnt;
             
-            if(sum1>target){
-                sum1-=arr[left++];
-                cal++;
+            if(sum1<target){
+                sum1+=arr[right++];
+                cnt++;
                 continue;
             }
             
-            sum1+=arr[right++];
-            cal++;
+            sum1-=arr[left++];
+            cnt++;
         }
-        
+        // 만약 target에 도달할 경우 cnt 리턴
         return -1;
-        
     }
 }
