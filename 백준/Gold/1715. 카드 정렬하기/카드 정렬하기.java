df1c7ae9abcc22703e0fab51c78850e226c71355
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-08-28 22:48:59 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 324 ms, Memory: 25396 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,38 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-		Queue<Integer> queue = new PriorityQueue<>();
-		for(int i=0;i<n;i++){
-			queue.add(Integer.valueOf(br.readLine()));
-		}
+        Queue<Integer> queue = new PriorityQueue<>();
 
-		if(n == 1){
-			System.out.println(0);
-			return;
-		}
+        for(int i=0;i<n;i++){
+            int val = Integer.parseInt(br.readLine());
+            queue.add(val);
+        }
 
-		int answer = 0;
+        int answer = 0;
+        while (!queue.isEmpty()){
+            int before = queue.poll();
+            if(queue.isEmpty()){
+                break;
+            }
+            int next = queue.poll();
 
-		while (queue.size()>1){
-			int currentGroup1  = queue.poll();
-			int currentGroup2 = queue.poll();
+            answer += next+before;
 
-			int totalCurrentGroup = currentGroup1+currentGroup2;
-			answer += totalCurrentGroup;
-			queue.add(totalCurrentGroup);
-		}
+            if(queue.isEmpty())
+                break;
+            queue.add(before+next);
+        }
 
-		System.out.println(answer);
+        System.out.println(answer);
 
-
-	}
+    }
 }
