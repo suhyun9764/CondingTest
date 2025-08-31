849549f3f293c614b81e3c8569139325f28f379a
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-01 00:35:09 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1068 ms, Memory: 172988 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,74 +1,34 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<Integer,Integer> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw  = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(st.nextToken()); // 총 카드의 개수
-        int[] arr = new int[n];
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){ // 카드에 적힌 수
-            arr[i] = Integer.parseInt(st.nextToken());  // 시간 복잡도 O(N)
+        for(int i=0;i<n;i++){
+            int cur = Integer.parseInt(st.nextToken());
+            map.put(cur,map.getOrDefault(cur,0)+1);
         }
 
-        Arrays.sort(arr);
-
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken()); // 비교해야 할 카드 개수
-        int[] ansArr = new int[m];
-        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<m;i++){
-            int target = Integer.parseInt(st.nextToken());
-            int lower = getLower(arr,target);
-            int upper = getUpper(arr,target);
-
-            ansArr[i] = upper - lower;
-        }
-
-
-        for(int i=0;i<ansArr.length;i++){
-            bw.write(ansArr[i]+" ");
-        }
-
-        bw.flush();
-    }
-
-    private static int getUpper(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length-1;
-        int index=arr.length;
-        while(start<=end){
-            int m = (start+end)/2;
-            if(arr[m]<=target) start = m+1;
-            else{
-                end = m-1;
-                index = m;
-                //return getSameNum(arr,m);
+            int cur = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(cur,0));
+            if(i<m-1){
+                sb.append(" ");
             }
         }
-        return index;
+
+        System.out.println(sb.toString());
+
     }
-
-    private static int getLower(int[] arr, int target) { // 시간 복잡도 O(logn)
-        int start = 0;
-        int end = arr.length-1;
-        int index=arr.length;
-        while(start<=end){
-            int m = (start+end)/2;
-            if(arr[m]<target) start = m+1;
-            else{
-                end = m-1;
-                index = m;
-                //return getSameNum(arr,m);
-            }
-        }
-        return index;
-    }
-
-
 }
