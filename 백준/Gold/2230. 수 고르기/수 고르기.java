4acbf39147629f76d94d7307c20987be642b4844
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-02 03:18:10 +0900

    [Gold V] Title: 수 고르기, Time: 336 ms, Memory: 27692 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -5,36 +5,34 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
         for(int i=0;i<n;i++){
-            st= new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
-        int end = 0;
-  
-        int min = arr[arr.length-1]-arr[0];
-        for(int i=0;i<n;i++){
-            while (arr[end]-arr[i]<m&&end<n-1){
-                end++;
-            }
-            
-            int value = arr[end]-arr[i];
-            if(value>=m){
-                if(value<min)
-                    min = value;
+
+        int left =0;
+        int right =0;
+
+        int answer = arr[n-1]-arr[0];
+        while(left<n&&right<n){
+            int dif = arr[right] - arr[left];
+            if(dif>=m){
+                answer = Math.min(answer,dif);
+                left++;
+            }else{
+                right++;
             }
         }
 
-        System.out.println(min);
+        System.out.println(answer);
     }
 }
