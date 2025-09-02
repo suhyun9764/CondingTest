86d53a9fa407b2f03145b91ffda1ea4c622a8d5c
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-02 21:05:10 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 79.66 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,50 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        Map<String, Integer> map = new HashMap<>();
         Set<String> set = new HashSet<>();
         for(String gem : gems){
             set.add(gem);
         }
         
-        int totalNum = set.size();
+        int n = set.size();
         
-        for(int i=0;i<totalNum;i++){
-            map.put(gems[i],map.getOrDefault(gems[i],0)+1);
-        }
-        
-        if(map.size()==totalNum)
-            return new int[]{1,totalNum};
-        
+        Map<String,Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 1;
         int min = gems.length;
-        int start = 0;
-        int end = gems.length-1;
+        int answerLeft = 0;
+        int answerRight = 0;
         
-        int startIndex = 0;
-        
-        for(int i=totalNum;i<gems.length;i++){
-            String currentGem = gems[i];
-            map.put(currentGem, map.getOrDefault(currentGem,0)+1);
+        for(int i=0;i<gems.length;i++){
+            map.put(gems[i],map.getOrDefault(gems[i],0)+1);
             
-            while(map.get(gems[startIndex])>1){
-                map.put(gems[startIndex], map.get(gems[startIndex])-1);
-                startIndex++;
-            }
-            
-            if(map.size()==totalNum){
-                if(min>i-startIndex+1){
-                    min = i-startIndex+1;
-                    start = startIndex;
-                    end = i;
-                }else if(min == i-startIndex+1){
-                    if(start>startIndex){
-                        start = startIndex;
-                        end = i;
-                    }
+            if(map.size()==n){
+                while(map.get(gems[left])>=2){
+                    map.put(gems[left],map.get(gems[left])-1);
+                    left++;
                 }
+                 if(i-left<min){
+                    min = i-left;
+                    answerLeft = left;
+                    answerRight = i;
+                 }
             }
+            
+           
         }
         
-        return new int[]{start+1, end+1};
+        return new int[]{answerLeft+1,answerRight+1};
     }
 }
