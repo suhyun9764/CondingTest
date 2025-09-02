9c8807ca50f52a5a11e8b3ed46f2d54825f6b10d
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-02 22:53:00 +0900

    [level 3] Title: 징검다리 건너기, Time: 173.51 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,43 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] stones, int k) {
-        // 어차피 모든 돌이 0이하로 되면 건너지못함
-        // 따라서 최소 = 1명, 최대 200000으로 지정
-        // 이분탐색
-        // mid값으로 뺀 배열에 K길이만큼 연속된 0이 있는지 확인
-        // 있다면 right = mid-1;
-        // 없다면 left = mid+1;
-        int max = 0;
-        for(int i=0;i<stones.length;i++){
-            if(stones[i]>max)
-                max = stones[i];
-        }
-        int left = 1;
-        int right = max;
+        int n = stones.length;
+        
+        int[] copy = Arrays.copyOf(stones,n);
+        Arrays.sort(copy);
+        int left = 0;
+        int right = copy[n-1];
         int answer = 0;
         while(left<=right){
             int mid = (left+right)/2;
-            if(isAvailable(mid,stones,k)){
+            if(isAvailable(stones,mid,k)){
+                left = mid+1;
                 answer = mid;
-                left=mid+1;
             }else{
                 right = mid-1;
             }
         }
         
-        return answer+1;
+        return answer;
     }
     
-    public boolean isAvailable(int mid, int[] stones, int k){
-        int cnt = 0;
-        for(int i=0;i<stones.length;i++){
-            if(stones[i]-mid<=0){
-                cnt++;
+    private boolean isAvailable(int[] stones, int mid, int k){
+        int zeroCnt = 0;
+        for(int stone : stones){
+            if(stone-mid<0){
+                zeroCnt++;
+                
+                if(zeroCnt>=k){
+                    return false;
+                }
             }else{
-                cnt = 0;
+                zeroCnt = 0;
             }
-            
-            if(cnt>=k)
-                return false;
         }
         
         return true;
