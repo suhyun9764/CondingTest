e17d3f832de777cde363bcba67bea8b2fc1d8ca4
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-02 22:20:49 +0900

    [Gold IV] Title: 공유기 설치, Time: 260 ms, Memory: 28508 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,50 +5,51 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int c= Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-
         for(int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int left =0;
+        int right = arr[arr.length-1]-arr[0];
+        int answer = 0;
+        while (left<=right){
+             int mid = (left+right)/2;
+             if(isAvailable(arr,mid,c)){
+                 left = mid+1;
+                 answer = mid;
+             }else{
+                 right = mid-1;
+             }
         }
 
-        Arrays.sort(arr);  // O(nlog(n))
-        int start = 1;
-        int end = arr[arr.length-1]-arr[0];
-        int ans =0;
-        while(start<=end){
-            int m = (start+end)/2;
-            int num = getCount(arr,m);
-            if(num<c){
-                end = m-1;
-            }else{
-                ans = m;
-                start = m+1;
-            }
-        }
+        System.out.println(answer);
 
-        System.out.println(ans);
+
     }
 
-    static int getCount(int[] arr, int m){
-        int pastPoint = arr[0];
-        int cnt =1;
+    private static boolean isAvailable(int[] arr, int mid,int c) {
+        int before = arr[0];
+        int cnt = 1;
         for(int i=1;i<arr.length;i++){
-            if(arr[i]-pastPoint>=m){
+            int cur = arr[i];
+            if(cur-before>=mid){
                 cnt++;
-                pastPoint = arr[i];
+                before = cur;
             }
         }
 
-        return cnt;
+        if(cnt >= c){
+            return true;
+        }
+
+        return false;
     }
 }
