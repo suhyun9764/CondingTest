2ded152c8a2300f96cf8e4050f828db6b3cf3cd0
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-05 02:18:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 108 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,20 +16,44 @@ public class Main {
         }
 
         int[] dp = new int[n];
-        int len = 0;
+//        int len = 0;
+//
+//        int answer = 0;
+//        for(int i=0;i<n;i++){
+//            dp[i] = 1;
+//            for(int j=0;j<i;j++){
+//                if(arr[j]<arr[i]){
+//                    dp[i] = Math.max(dp[i],dp[j]+1);
+//                }
+//            }
+//            answer = Math.max(dp[i],answer);
+//        }
+//
+//        System.out.println(answer);
 
-        int answer = 0;
+        int len =0;
         for(int i=0;i<n;i++){
-            dp[i] = 1;
-            for(int j=0;j<i;j++){
-                if(arr[j]<arr[i]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
-            }
-            answer = Math.max(dp[i],answer);
+            int cur = arr[i];
+            int pos = lowerBound(dp,0,len,cur);
+            dp[pos] = cur;
+            if(pos==len)
+                len++;
         }
 
-        System.out.println(answer);
+        System.out.println(len);
 
+    }
+
+    private static int lowerBound(int[] dp, int left, int right, int target) {
+        while (left<right){
+            int mid = (right+left)/2;
+            if(dp[mid]<target){
+                left = mid+1;
+            }else{
+                right=mid;
+            }
+        }
+
+        return left;
     }
 }
