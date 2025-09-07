23918fb617eabda289a2c54d5186a9dd297ee85b
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-07 22:11:04 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 3.79 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,74 +1,54 @@
 import java.util.*;
 
 class Solution {
-        public int solution(String str1, String str2) {
-            // 1. str1 set에 저장
-            // 요구사항 : 대소문자 구분 x, 특수문자 포함하면 해당 글자 쌍 버리기
-            Map<String,Integer> str1Map = new HashMap<>();
-            char[] str1CharArray = str1.toCharArray();
-            Character before = null;
-            int str1Count = 0;
-            for(int i=0;i<str1CharArray.length;i++){
-                if(before==null){
-                    char current = str1CharArray[i];
-                    if(!Character.isLetter(current)) continue;
-                    current = Character.toLowerCase(current);
-                    before = current;
-                    continue;
-                }
-
-                Character current = str1CharArray[i];
-                if(!Character.isLetter(current)){
-                    before = null;
-                    continue;
-                }
-                current = Character.toLowerCase(current);
-                String comb = before.toString() + current.toString();
-                str1Map.put(comb, str1Map.getOrDefault(comb,0)+1);
-                str1Count++;
-                before = current;
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        char[] charArr1 = str1.toCharArray();
+        char[] charArr2 = str2.toCharArray();
+        
+        double aNum = 0;
+        double bNum = 0;
+        double both = 0;
+        
+        Map<String,Integer> map1 = new HashMap<>();
+        Map<String,Integer> map2 = new HashMap<>();
+        for(int i=0;i<charArr1.length-1;i++){
+            StringBuilder sb = new StringBuilder();
+            if(Character.isAlphabetic(charArr1[i])&&Character.isAlphabetic(charArr1[i+1])){
+                sb.append(charArr1[i]);
+                sb.append(charArr1[i+1]);
+                String lowerCase = sb.toString().toLowerCase();
+                map1.put(lowerCase,map1.getOrDefault(lowerCase,0)+1);
+                aNum ++;
             }
-            // 2. str2 set에 저장
-            // 요구사항1 : 대소문자 구분 x, 특수문자 포함하면 해당 글자 쌍 버리기
-            Map<String,Integer> str2Map = new HashMap();
-            char[] str2CharArray = str2.toCharArray();
-            Character before2 = null;
-            int str2Count = 0;
-            for(int i=0;i<str2CharArray.length;i++){
-                if(before2==null){
-                    char current = str2CharArray[i];
-                    if(!Character.isLetter(current)) continue;
-                    current = Character.toLowerCase(current);
-                    before2 = current;
-                    continue;
-                }
-
-                Character current = str2CharArray[i];
-                if(!Character.isLetter(current)){
-                    before2 = null;
-                    continue;
-                }
-                current = Character.toLowerCase(current);
-                String comb = before2.toString() + current.toString();
-                str2Map.put(comb, str2Map.getOrDefault(comb,0)+1);
-                str2Count++;
-                before2 = current;
-            }
-
-            // 3. str2 map 순회하며 str1 set에 저장되어있는지 확인
-            int both = 0;
-            for (Map.Entry<String, Integer> str2Entry : str2Map.entrySet()) {
-                String key = str2Entry.getKey();
-                if(str1Map.containsKey(key))
-                    both += Math.min(str1Map.get(key),str2Entry.getValue());
-            }
-
-            int sum = str1Count + str2Count-both;
-            // 5. 합집합 갯수 구하기 : str1 map size + str2 map size - both
-            float result;
-            if(sum == 0) result = 1;
-            else result = (float) both / sum;
-            // 6. 자카드 유사도 계산
-             return (int) ((int)65536*result);
+        
         }
+        
+        for(int i=0;i<charArr2.length-1;i++){
+            StringBuilder sb = new StringBuilder();
+            if(Character.isAlphabetic(charArr2[i])&&Character.isAlphabetic(charArr2[i+1])){
+                sb.append(charArr2[i]);
+                sb.append(charArr2[i+1]);
+                String lowerCase = sb.toString().toLowerCase();
+                map2.put(lowerCase,map2.getOrDefault(lowerCase,0)+1);
+                bNum ++;
+            }
+            
+        }
+        
+        for(String key : map1.keySet()){
+            System.out.println(key);
+            both += Math.min(map1.get(key),map2.getOrDefault(key,0));
+        }
+         
+        double rate = 0;
+        
+        if(aNum==0&&bNum==0){
+            rate = 1;
+        }else{
+            rate =  both/(aNum+bNum-both);
+        }
+        
+        return (int)(Math.floor(rate*65536));
     }
+}
