69c41cad28f37570a2027045d1ca6eb12980f370
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-09-30 23:25:39 +0900

    [level 1] Title: 신고 결과 받기, Time: 273.46 ms, Memory: 204 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,54 +1,35 @@
-import java.util.*;  
+import java.util.*;
 
 class Solution {
-        static Map<String,Integer> ids = new HashMap<>();
-        static int[] reported;
-        static Set<Integer>[] reportList;
-        static int[] answer;
         public int[] solution(String[] id_list, String[] report, int k) {
-            // 1. 아이디들을 맵에 인덱스와 함께 저장하기
-            int idx = 0;
-            for (String id : id_list) {
-                ids.put(id, idx++);
+            Map<String, Set<String>> declared = new HashMap<>();
+            Map<String, Set<String>> declare = new HashMap<>();
+            for(int i=0;i<id_list.length;i++){
+                declared.put(id_list[i],new HashSet<>());
+                declare.put(id_list[i],new HashSet<>());
             }
 
-            reported = new int[idx];
-            reportList = new Set[idx];
-            for(int i=0;i< reportList.length;i++){
-                reportList[i] = new HashSet<>();
-            }
-            answer = new int[idx];
-            // 2. 신고 내역 순회
-            for (String eachReport : report) {
-                // 신고자와 피신고자 이름 분리
-                String[] names = eachReport.split(" ");
-                // 신고자와 피신고자 인덱스 가져오기
-                int reporterIdx = ids.get(names[0]);
-                int reportedIdx = ids.get(names[1]);
-                // 만약 같은 사람이 신고한 전적이 있으면 스킵
-                if(reportList[reporterIdx]!=null&&reportList[reporterIdx].contains(reportedIdx))
-                    continue;
-                //  repotred배열에 피신고자 인덱스 자리에 +1
-                reported[reportedIdx]++;
-                // reportList에 신고자 인덱스 자리에 피신고자 인덱스 넣기
-                reportList[reporterIdx].add(reportedIdx);
+            for(int i=0;i<report.length;i++){
+                String curReport = report[i];
+                String declarePerson = curReport.split(" ")[0];
+                String declaredPerson = curReport.split(" ")[1];
+
+                declare.get(declarePerson).add(declaredPerson);
+                declared.get(declaredPerson).add(declarePerson);
             }
 
-            // 3. 이용정지 당하는 유저 구하기
-            List<Integer> suspendUsers = new ArrayList<>();
-            for (int i=0;i<reported.length;i++) {
-                if(reported[i]>=k)
-                    suspendUsers.add(i);
-            }
-
-            for (int i=0;i<reportList.length;i++) {
-                int cnt = 0;
-                for (int suspendUser : suspendUsers) {
-                    if(reportList[i].contains(suspendUser))
-                        cnt++;
+            int[] answer = new int[id_list.length];
+            for(int i=0;i<id_list.length;i++){
+                String user = id_list[i];
+                Set<String> declaredUsers = declare.get(user);
+                for(int d=0;d<id_list.length;d++){
+                    String curUser = id_list[d];
+                    if(!declaredUsers.contains(curUser)) continue;
+                    if(declared.get(curUser).size()>=k)
+                        answer[i]++;
                 }
-                answer[i] = cnt;
             }
+
             return answer;
         }
     }
