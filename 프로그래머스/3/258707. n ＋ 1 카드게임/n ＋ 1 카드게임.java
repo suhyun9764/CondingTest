51f142ef4f913a46b93d3ee95351c8a15656c608
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-10-07 03:21:12 +0900

    [level 3] Title: n + 1 카드게임, Time: 12.00 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,85 +1,88 @@
 import java.util.*;
 
 class Solution {
-        public int solution(int coin, int[] cards) {
-            int n = cards.length;
-            int target = n + 1;
-            
-            // 초기 손패
-            Set<Integer> hand = new HashSet<>();
-            for (int i = 0; i < n / 3; i++) {
-                hand.add(cards[i]);
-            }
-            
-            // 뽑은 카드 보관소
-            Set<Integer> picked = new HashSet<>();
-            
-            int round = 1;
-            int cardIndex = n / 3;
-            
-            while (cardIndex < n) {
-                // 2장 뽑기
-                picked.add(cards[cardIndex]);
-                picked.add(cards[cardIndex + 1]);
-                cardIndex += 2;
-                
-                boolean canContinue = false;
-                
-                // 1. 손패에서만 페어를 만들 수 있는 경우 (코인 0개)
-                Integer pair = findPairInSet(hand, target);
-                if (pair != null) {
-                    hand.remove(pair);
-                    hand.remove(target - pair);
-                    canContinue = true;
-                }
-                // 2. 손패 1장 + picked 1장으로 페어 (코인 1개)
-                else if (coin >= 1) {
-                    pair = findPairBetweenSets(hand, picked, target);
-                    if (pair != null) {
-                        hand.remove(pair);
-                        picked.remove(target - pair);
-                        coin--;
-                        canContinue = true;
-                    }
-                    // 3. picked에서만 페어를 만들 수 있는 경우 (코인 2개)
-                    else if (coin >= 2) {
-                        pair = findPairInSet(picked, target);
-                        if (pair != null) {
-                            picked.remove(pair);
-                            picked.remove(target - pair);
-                            coin -= 2;
-                            canContinue = true;
-                        }
-                    }
-                }
-                
-                if (!canContinue) {
-                    break;
-                }
-                
-                round++;
-            }
-            
-            return round;
-        }
-        
-        // 한 세트 내에서 페어 찾기
-        private Integer findPairInSet(Set<Integer> set, int target) {
-            for (int card : set) {
-                if (set.contains(target - card)) {
-                    return card;
-                }
-            }
-            return null;
-        }
-        
-        // 두 세트 사이에서 페어 찾기
-        private Integer findPairBetweenSets(Set<Integer> set1, Set<Integer> set2, int target) {
-            for (int card : set1) {
-                if (set2.contains(target - card)) {
-                    return card;
-                }
-            }
-            return null;
-        }
-    }
+		public int solution(int coin, int[] cards) {
+			int target = cards.length+1;
+			Set<Integer> hand = new HashSet<>();
+			Set<Integer> pocket = new HashSet<>();
+			for(int i=0;i<cards.length/3;i++){
+				hand.add(cards[i]);
+			}
+
+			int round = 1;
+			int cardIndex = cards.length/3;
+			while (cardIndex<cards.length){
+				pocket.add(cards[cardIndex]);
+				pocket.add(cards[cardIndex+1]);
+				cardIndex+=2;
+
+				// 손에서만 가능한면 손에서 해결
+				Integer pair = findInHand(hand,target);
+				if(pair!=null){
+					hand.remove(pair);
+					hand.remove(target-pair);
+					round++;
+					continue;
+				}
+
+				Integer handPair = findInBoth(hand,pocket,target);
+				if(handPair!=null&&coin>=1){
+					coin--;
+					hand.remove(handPair);
+					pocket.remove(target-handPair);
+					round++;
+					continue;
+				}
+
+				Integer pocketPair = findInPocket(pocket,target);
+				if(pocketPair!=null&&coin>=2){
+					coin-=2;
+					pocket.remove(pocketPair);
+					pocket.remove(target-pocketPair);
+					round++;
+					continue;
+				}
+
+				break;
+
+				// 손 1장 주어진거 1장으로 해결
+				// 손에서불가능하다면 2장 가져오기
+			}
+			return round;
+		}
+
+		private Integer findInHand(Set<Integer> hand, int target) {
+			ArrayList<Integer> cards = new ArrayList<>(hand);
+			for(int i=0;i<cards.size();i++){
+				int curCard = cards.get(i);
+				if(hand.contains(target-curCard)){
+					return curCard;
+				}
+			}
+			return null;
+		}
+
+		private Integer findInBoth(Set<Integer> hand, Set<Integer> pocket, int target) {
+			ArrayList<Integer> pocketCards = new ArrayList<>(pocket);
+			ArrayList<Integer> handCards = new ArrayList<>(hand);
+
+			for(int i=0;i<handCards.size();i++){
+				int curCard = handCards.get(i);
+				if(pocket.contains(target-curCard)){
+					return curCard;
+				}
+			}
+			return null;
+		}
+
+		private Integer findInPocket(Set<Integer> pocket, int target) {
+			ArrayList<Integer> cards = new ArrayList<>(pocket);
+			for(int i=0;i<cards.size();i++){
+				int curCard = cards.get(i);
+				if(pocket.contains(target-curCard)){
+					return curCard;
+				}
+			}
+			return null;
+		}
+	}
