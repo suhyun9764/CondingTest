9548703c6046277e4f612bd75edce6a040f29dc7
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-10-10 08:44:33 +0900

    [level 3] Title: 합승 택시 요금, Time: 37.01 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=java) 
 
 ### 성능 요약
 
-메모리: 69.5 MB, 시간: 34.23 ms
+메모리: 69.6 MB, 시간: 37.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 07:57:46
+2025년 10월 10일 08:43:33
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,60 +1,72 @@
 import java.util.*;
 
 class Solution {
-		int N;
-		public int solution(int n, int s, int a, int b, int[][] fares) {
-			N = n;
-			int[][] map = new int[n+1][n+1];
-			for(int i=0;i<n+1;i++){
-				Arrays.fill(map[i],-1);
-			}
-
-			for(int i=0;i<fares.length;i++){
-				int start = fares[i][0];
-				int end= fares[i][1];
-
-				map[start][end] = fares[i][2];
-				map[end][start] = fares[i][2];
-			}
-
-
-			// 환승지점 h : (a+h)+(h+a)+(h+b) 최소값 구하기
-			int[] sMap = getMinPrice(s,map);
-			// 2. 출발지에서 B로 가는 최단 경로
-			int[] aMap = getMinPrice(a,map);
-			// 3. A에서 B로 가는 최단 경로
-			int[] bMap= getMinPrice(b,map);
-
-			int answer = Integer.MAX_VALUE;
-			for(int i=1;i<N+1;i++){
-				int sum = sMap[i] + aMap[i] + bMap[i];
-				answer=Math.min(answer, sum);
-			}
-			// 1+2 or 1+3 or 2+3
-			return answer;
-		}
-
-		private int[] getMinPrice(int a, int[][] map) {
-			int[] curMap = new int[N+1];
-			Arrays.fill(curMap,Integer.MAX_VALUE);
-			curMap[a] = 0;
-			Queue<Integer> queue = new ArrayDeque<>();
-			queue.add(a);
-
-			while (!queue.isEmpty()){
-				int cur = queue.poll();
-
-				for(int i=1;i<N+1;i++){
-					if(i==cur) continue;
-					if(map[cur][i]!=-1&&curMap[i]>curMap[cur]+map[cur][i]){
-						queue.add(i);
-						curMap[i] = curMap[cur]+map[cur][i];
-					}
-				}
-			}
-
-			return curMap;
-
-
-		}
-	}
+    int n;
+    int[][] prices;
+    int[][] minimumPrices;
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        // 1. s->a + s-> b
+        // 2. s->a + a ->b
+        // 3. s->b + b ->a
+        this.n = n;
+        prices = new int[n+1][n+1];
+        for(int[] fare : fares){
+            int start = fare[0];
+            int end = fare[1];
+            int price = fare[2];
+            
+            prices[start][end] = price;
+            prices[end][start] = price;
+        }
+        
+        int answer = Integer.MAX_VALUE;
+        minimumPrices = new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            Arrays.fill(minimumPrices[i],-1); 
+        }
+        getMinimumPriceGraph(s);
+        getMinimumPriceGraph(a);
+        getMinimumPriceGraph(b);
+        
+        for(int i=1;i<=n;i++){
+            int sToI = minimumPrices[s][i];
+            // if(minimumPrices[s][i]==-1||minimumPrices[a][i]==-1||minimumPrices[i][b]==-1||minimumPrices[b][i]==-1){
+            //     getMinimumPriceGraph(i);
+            // }
+            int iToA = minimumPrices[a][i];
+            int iToB = minimumPrices[b][i];
+            answer = Math.min(sToI+iToA+iToB,answer);
+        }
+        
+        return answer;
+    }
+    
+    
+    private void getMinimumPriceGraph(int start){
+        int[] targets = new int[n+1];
+        Arrays.fill(targets,Integer.MAX_VALUE);
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        targets[start] = 0;
+        boolean[] visited = new boolean[n+1];
+        // visited[start] = true;
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            
+            for(int i=1;i<=n;i++){
+                if(i==cur) continue;
+                if(prices[cur][i]==0) continue;
+                if(targets[i]<=targets[cur]+prices[cur][i]) continue;
+                if(visited[i]) continue;
+                // visited[i] = true;
+                queue.add(i);
+                
+                targets[i] = targets[cur]+prices[cur][i];
+            }
+        }
+        for(int i=1;i<=n;i++){
+            minimumPrices[start][i] = targets[i];
+            minimumPrices[i][start] = targets[i];
+        }
+    }
+}
