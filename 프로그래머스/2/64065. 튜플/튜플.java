334bd8ab87c3f79edc152e75003639107a4be805
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-10-10 04:50:39 +0900

    [level 2] Title: 튜플, Time: 89.90 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,31 +1,58 @@
 import java.util.*;
 
 class Solution {
-        public int[] solution(String s) {
-            s = s.substring(2, s.length() - 2);
-            String[] tuples = s.split("},\\{");
-            Arrays.sort(tuples, new Comparator<String>() {
-                public int compare(String o1, String o2) {
-                    return o1.split(",").length - o2.split(",").length;
-                }
-            });
-
-            int length = tuples[tuples.length - 1].split(",").length;
-            int[] answer = new int[length];
-            Set<Integer> set = new HashSet<>();
-            for (int i = 0; i < tuples.length; i++) {
-                String tuple = tuples[i];
-                String[] values = tuple.split(",");
-                for (String strValue : values) {
-                    int value = Integer.parseInt(strValue);
-                    if (set.contains(value)) continue;
-                    set.add(value);
-                    answer[i] = value;
-                    break;
-                }
+    public int[] solution(String s) {
+        // 해당 인덱스 = 전체 원소 수 - map에 저장된 수
+        List<List<Integer>> info = getInfo(s);    
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        
+        int[] answer = new int[info.size()];
+        
+        for(List<Integer> l : info){
+            for(int i : l){
+                map.put(i,map.getOrDefault(i,0)+1);
             }
-
-            return answer;
+        }
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            int value = entry.getKey();
+            int num = entry.getValue();
+            
+            int index = info.size()-num;
+            answer[index] = value;
+        }
+        return answer;
+    }
+    
+    private List<List<Integer>> getInfo(String s){
+        /*
+        {1,2,3},{2,1},{1,2,4,3},{2}
+        */
+        
+        List<List<Integer>> list = new ArrayList<>();
+        s = s.substring(1,s.length()-1);// 밖에 있는 괄호 제거
+        List<Integer> atoms = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='{') continue;
+            if(c==',') {
+                if(sb.length()==0) continue;
+                atoms.add(Integer.parseInt(sb.toString()));
+                sb.setLength(0);
+            }
+            if(c=='}'){
+                atoms.add(Integer.parseInt(sb.toString()));
+                sb.setLength(0);
+                list.add(atoms);
+                atoms = new ArrayList<>();
+            }
+            if(Character.isDigit(c)){
+                sb.append(c);
+            }
         }
 
+        return list;
     }
+}
