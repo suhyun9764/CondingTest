e8ddb123c5bfc7a73fbc3f29eef09776144def35
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-11-13 17:45:53 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,6 +1,27 @@
 class Solution {
+    boolean[] visit;
+    int[][] computers;
     public int solution(int n, int[][] computers) {
+        this.visit = new boolean[n];
+        this.computers = computers;
         int answer = 0;
+        for(int i=0;i<n;i++){
+            if(visit[i]) continue;
+            visit[i] = true;
+            dfs(i);
+            answer++;
+        }
         return answer;
+    }
+    
+    private void dfs(int i){
+        int[] nodes = computers[i];
+        for(int node = 0; node<nodes.length;node++){
+            if(node==i) continue;
+            if(nodes[node]==0) continue;
+            if(visit[node]) continue;
+            visit[node] = true;
+            dfs(node);
+        }
     }
 }
