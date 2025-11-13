13225cdce0128cd8f5bed0c0a4dd2724be255427
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-11-13 18:04:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.41 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,47 @@
 import java.util.*;
+
 class Solution {
-    static int[][] maps;
+    boolean[][] visit;
+    int[] dy = {-1,1,0,0};
+    int[] dx = {0,0,-1,1};
+    int n,m;
     public int solution(int[][] maps) {
-        this.maps = maps;
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{0,0,1});
-        return bfs(queue);
-    }
-    
-    
-    int bfs(Queue<int[]> queue){ 
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        n = maps.length;
+        m = maps[0].length;
+        visit = new boolean[n][m];
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.add(new Pos(0,0,1));
+        int answer = -1;
         while(!queue.isEmpty()){
-            int[] current = queue.poll();
-            int y = current[0];
-            int x = current[1];
-            int count = current[2];
+            Pos cur = queue.poll();
+            if(cur.y==n-1&&cur.x==m-1) {
+                answer = cur.order;
+                break;
+            }
             
-            if(y==maps.length-1&&x==maps[0].length-1)
-                return count;
- 
-            int[] dy = {-1,1,0,0};
-            int[] dx = {0,0,-1,1};
-        
             for(int d=0;d<4;d++){
-                int nY = y+dy[d];
-                int nX = x+dx[d];
-            
-                if(nY<0||nY>=maps.length||nX<0||nX>=maps[0].length||visited[nY][nX]||maps[nY][nX]==0) continue;
-                visited[nY][nX] = true;
-                queue.add(new int[]{nY,nX,count+1});
+                int ny = cur.y+dy[d];
+                int nx = cur.x+dx[d];
+                if(ny<0||ny>=n||nx<0||nx>=m) continue;
+                if(visit[ny][nx]) continue;
+                if(maps[ny][nx]==0) continue;
+                visit[ny][nx] = true;
+                queue.add(new Pos(ny,nx,cur.order+1));
             }
         }
-        return -1;
+        
+        return answer;
+    }
+}
+
+class Pos{
+    int y;
+    int x;
+    int order;
+    
+    public Pos(int y,int x, int order){
+        this.y = y;
+        this.x = x;
+        this.order = order;
     }
 }
