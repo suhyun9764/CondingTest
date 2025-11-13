1598da3dd81000ee83e86e9ea9c45ecf9050d32b
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-11-14 00:43:44 +0900

    [level 3] Title: 단어 변환, Time: 0.22 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,36 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
-        int count;
-        public int solution(String begin, String target, String[] words) {
-            count = words.length;
-            Set<String> set = new HashSet<>();
-            for (String word : words) {
-                set.add(word);
-            }
-            if(!set.contains(target))
-                return 0;
-
-            dfs(begin,target,words,0,set);
-            return count;
+    int answer = Integer.MAX_VALUE;
+    public int solution(String begin, String target, String[] words) {
+        boolean[] visit = new boolean[words.length];
+        dfs(begin, target, words, 0, new boolean[words.length]);
+        if(answer==Integer.MAX_VALUE) return 0;
+        return answer;
+    }
+    
+    private void dfs(String cur, String target, String[] words, int depth, boolean[] visit){
+        if(cur.equals(target)){
+            answer= Math.min(answer,depth);
+            return;
         }
-
-        private void dfs(String current, String target, String[] words, int depth, Set set) {
-            if(current.equals(target)){
-                count = Math.min(count,depth);
-                return;
-            }
-
-            for(int i=0;i<words.length;i++){
-                String thisWord = words[i];
-                if(!set.contains(thisWord)) continue;
-                if(compareWord(current,thisWord)==1){
-                    set.remove(thisWord);
-                    dfs(thisWord,target,words,depth+1,set);
-                    set.add(thisWord);
-                }
-            }
-        }
-
-        private int compareWord(String current, String thisWord) {
-            int count = 0;
-            char[] currentArray = current.toCharArray();
-            char[] thisArray = thisWord.toCharArray();
-
-            for(int i=0;i<currentArray.length;i++){
-                if(currentArray[i]!=thisArray[i])
-                    count++;
-            }
-
-            return count;
+        // System.out.println(cur);
+        for(int i=0;i<words.length;i++){
+            if(visit[i]) continue;
+            if(!isOneDiff(cur,words[i])) continue;
+            visit[i] = true;
+            dfs(words[i],target,words,depth+1,visit);
+            visit[i] = false;
         }
     }
+    
+    private boolean isOneDiff(String s1, String s2){
+        int diff = 0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)) diff++;
+            if(diff>1) return false;
+        }
+        
+        return diff==1;
+    }
+}
