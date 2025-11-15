5e1d534f412f4ac6b02d95165bfd86f026160e75
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-11-15 22:59:46 +0900

    [Gold V] Title: 평범한 배낭, Time: 148 ms, Memory: 15576 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,19 +1,18 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         int[] dp = new int[K+1];
-        for (int i = 0; i < N; i++) {
+        for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
@@ -24,7 +23,6 @@ public class Main {
         }
 
         System.out.println(dp[K]);
+
     }
-
 }
-
