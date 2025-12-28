708ebe168f6765cc68f5ade6c065b4d98fae67e3
suhyun <80302833+suhyun9764@users.noreply.github.com>
2025-12-29 00:37:37 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 96 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonachi(n));
+
+
+    }
+
+    private static long fibonachi(int n) {
+        long[] memo = new long[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            memo[i] = memo[i-2]+memo[i-1];
+        }
+
+        return memo[n];
+    }
+}
