90cffe65a4fc677283ff7c6ebe5f953d0602e6f4
suhyun <80302833+suhyun9764@users.noreply.github.com>
2026-03-18 19:23:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.26 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 98.9 MB, 시간: 106.70 ms
+메모리: 98.6 MB, 시간: 90.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 19일 09:52:25
+2026년 03월 18일 19:23:52
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,27 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-        public String solution(String[] participant, String[] completion) {
-            Map<String,Integer> map = new HashMap<>();
-            for (String name : participant) {
-                map.put(name,map.getOrDefault(name,0)+1);
-            }
-
-            for (String name : completion) {
-                map.put(name, map.get(name)-1);
-            }
-
-            for (String name : map.keySet()) {
-                if(map.get(name)!=0)
-                    return name;
-                
-            }
-            return null;
+    public String solution(String[] participant, String[] completion) {
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<completion.length;i++){
+            map.put(completion[i],map.getOrDefault(completion[i],0)+1);
         }
+        
+        String answer = null;
+        for(int i=0;i<participant.length;i++){
+            String cur = participant[i];
+            if(!map.containsKey(cur)){
+                answer = cur;
+                break;
+            }
+            if(map.containsKey(cur)&&map.get(cur)==0){
+                answer = cur;
+                break;
+            }
+            
+            map.put(cur,map.get(cur)-1);
+        }
+        
+        return answer;
     }
+}
